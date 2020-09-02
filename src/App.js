332ae20ee34e021647c6fbe520cd81f58743c958
Vasil Hadzi-Tosev <vasil@stech.com>
2020-09-02 18:04:01 +0200

    Specifying Children with JSX

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function formatName(user) {
   return user.firstName + " " + user.lastName;
 }
 
-const element = <h1 className="centered">Hello, {formatName(user)}!</h1>;
+const element = <h1>Hello, {formatName(user)}!</h1>;
 
 
 function getGreeting(user){
@@ -26,9 +26,18 @@ function getGreeting(user){
 const element1 = <div tabIndex="0"></div>;
 
 const element2 = <img src={user.avatarUrl}></img>;
+/*OR, if a tag is empty, we can just close it with />*/
+const element3 = <img src={user.avatarUrl}/>
+
+const element4 = (
+  <div>
+    <h1 className="centered">Hello, {formatName(user)}!<br/></h1>
+    <h2 className="centered">Good to see you here.</h2>
+  </div>
+);
 
 function App() {
-  return <>{element}</>;
+  return <>{element4}</>;
 }
 
 export default App;
