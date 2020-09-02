913137f66c99ac8a335410a2a75c4134cd7176bc
Vasil Hadzi-Tosev <vasil@stech.com>
2020-09-02 14:54:06 +0200

    added hello, name, removed old h1
    second description

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import './App.css';
+import ReactDOM from 'react-dom';
 
-const element = <h1 className="centered">Hello, world!</h1>;
+/*const element = <h1 className="centered">Hello, world!</h1>;*/
 
-/*const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>;*/
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
 
 function App() {
 
