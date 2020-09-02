25c9306a90dad224603b39f46288a2ed060ee19c
Vasil Hadzi-Tosev <vasil@stech.com>
2020-09-02 15:02:05 +0200

    Third update to the code
    Also, third description. Embed the result of calling a JavaScript funtion.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import ReactDOM from 'react-dom';
 
 /*const element = <h1 className="centered">Hello, world!</h1>;*/
 
-const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>;
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1 className="centered">
+    Hello, {formatName(user)}!
+  </h1>
+);
 
 ReactDOM.render(
   element,
