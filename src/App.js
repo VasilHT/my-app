032225f9736e6f5f9006b0097c5e92d3ba9c354a
Vasil Hadzi-Tosev <vasil@stech.com>
2020-09-03 15:08:32 +0200

    small changes

--- src/App.js
+++ src/App.js
@@ -87,6 +87,32 @@ class Welcome extends React.Component{
 }
 */
 
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
 function App(){
   return (
     <div>
@@ -96,9 +122,9 @@ function App(){
     </div>
   );
 }
-/*
-function App() {
-  return <>{elementAB}</>;
-}
-*/
+
+// function App() {
+//   return <>{elementAB}</>;
+// }
+
 export default App;
