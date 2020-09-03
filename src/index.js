d329701b0b062a97714721728cd387a67833a6be
Ertan Weezboo <ertan@stech.com>
2020-09-03 14:04:24 +0100

    comment component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Comment from "./components/Comment";
+import * as serviceWorker from "./serviceWorker";
+import "./App.css";
+
+// comment data
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React!",
+  author: {
+    name: "Hello Kitty",
+    avatarUrl: "https://placekitten.com/g/64/64",
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
+    <Comment date={comment.date} text={comment.text} author={comment.author} />,
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
 //ReactDOM.render(Element, document.getElementById('root'));
 
